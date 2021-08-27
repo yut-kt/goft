21400471888c2523ac162c6421823980ae1b7c55
yut-kt <yut.kt59@gmail.com>
2021-08-27 19:57:00 +0900

    update: move interface parse func

--- dft.go
+++ dft.go
@@ -1,10 +1,8 @@
 package goft
 
 import (
-	"errors"
 	"math"
 	"math/cmplx"
-	"reflect"
 )
 
 func DFT(f interface{}) ([]complex128, error) {
@@ -69,23 +67,4 @@ func idftMapping(F []complex128, x float64) complex128 {
 		sumImag += real(F[t])*sinV + imag(F[t])*cosV
 	}
 	return complex(sumReal/N, sumImag/N)
-}
-
-func interfaceToComplexSlice(i interface{}) ([]complex128, error) {
-	if reflect.ValueOf(i).Kind() != reflect.Slice {
-		return nil, errors.New("interface is not slice")
-	}
-
-	var cSlice []complex128
-	switch slice := i.(type) {
-	case []complex128:
-		cSlice = append(cSlice, slice...)
-	case []float64:
-		for _, v := range slice {
-			cSlice = append(cSlice, complex(v, 0))
-		}
-	default:
-		return nil, errors.New("unsupported type")
-	}
-	return cSlice, nil
 }
